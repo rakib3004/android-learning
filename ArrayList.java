53f7e69251ec46528be6c43f215248286ec7cbac
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-01-25 08:24:49 +0600

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,18 @@
+package IOStream;
+
+public class ArrayList {
+    public static void main(String[] args) {
+
+
+        String string = " Rakib";
+        String string1 = string.substring(1,3);
+        int length = string.length();
+        String string2 = string.substring(1,length);
+        String string3 = string.substring(0, 4);
+        System.out.println(string);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+    }
+
+}
---
+++ OutStream.java
@@ -0,0 +1,15 @@
+package IOStream;
+
+import java.io.*;
+
+import java.io.IOException;
+
+public class OutStream {
+
+
+    public static void main(String [] args) throws IOException{
+Processing processing = new Processing();
+processing.fileReader();
+    }
+
+}
